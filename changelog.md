2c56e29bc7876fdd2f9d9235e4c72d2370c285a5
Douglas Calora <douglascalora123@gmail.com>
2021-09-29 12:00:25 -0300

    Releasing v0.1.0

--- changelog.md
+++ changelog.md
@@ -2,7 +2,7 @@
 All notable changes in this project will be documented in this file.
 The format is based on Keep a Changelog, and this project adheres to Semantic Version Control.
 
-## 0.1.0 - 2021-09-28
+## 0.1.0 - 2021-09-29
 ### Added
   - Insertion of trisul in the list of companies.
 
--- dist/index.js
+++ dist/index.js
@@ -6,34 +6,40 @@
 
   const companies = {
     bild: {
-      brand: require('../assets/brands/bild'),
+      brand: require('../assets/brands/bild.svg'),
       color: '#ed7203',
       name: 'Bild',
       domains: ['bild.com.br']
     },
     exklusiv: {
-      brand: require('../assets/brands/exklusiv'),
+      brand: require('../assets/brands/exklusiv.svg'),
       color: '#3e3a35',
       name: 'Bild Exklusiv',
       domains: ['bildexklusiv.com.br']
     },
     vitta: {
-      brand: require('../assets/brands/vitta'),
+      brand: require('../assets/brands/vitta.svg'),
       color: '#006cbb',
       name: 'Vitta Residencial',
       domains: ['attivcorrespondente.com.br', 'vendasvitta.com.br', 'vittaconsorcio.com.br', 'vittaresidencial.com.br']
     },
     fegik: {
-      brand: require('../assets/brands/fegik'),
+      brand: require('../assets/brands/fegik.svg'),
       color: '#132f4d',
       name: 'Fegik',
       domains: ['fegik.com.br']
     },
     veraz: {
-      brand: require('../assets/brands/veraz'),
+      brand: require('../assets/brands/veraz.svg'),
       color: '#e66d2d',
       name: 'Veraz',
       domains: ['verazseguros.com.br']
+    },
+    trisul: {
+      brand: require('../assets/brands/trisul.svg'),
+      color: '#0d2440',
+      name: 'Trisul',
+      domains: ['portal-trisul.nave.dev']
     }
   };
 
